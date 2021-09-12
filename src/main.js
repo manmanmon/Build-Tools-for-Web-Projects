48973f8950eb0526bdf9b92df83c8252a1d3b94c
menmanmon <burgoch-roman9a@mail.ru>
2021-09-12 21:54:49 +0300

    Стили добавляются в сборку отдельным файлом
    Звук до сих пор не воспроизводится по окончанию таймера

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { handleCalcDates } from "./datecalc.js"
 import { startTimer } from "./timer.js"
 import './style.css'
+import { chunk } from "lodash/chunk"
+console.log(chunk);
+
 const dateCalcForm = document.getElementById("datecalc");
 const timerRun = document.getElementById("timer_run");
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,28 @@
 const path = require("path")
 const HTMLWebpackPlugin = require("html-webpack-plugin") //подключаем плагин в вебпак (подключаем класс, поэтому с большой буквы)
-const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+const OptimizeCssAssetWebpackPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserWebpackPlugin = require("terser-webpack-plugin")
+const { config } = require("process")
+
+const isDev = process.env.NODE_ENV === "development"
+const isProd = !isDev
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: "all"
+        }
+    }
+    if (isProd){
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
 
 module.exports = {
     context: path.resolve(__dirname, "src"),
@@ -10,18 +32,36 @@ module.exports = {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
+    devServer: {
+        port: 4200,
+        hot: isDev
+    },
+    optimization: optimization(),
     plugins: [
         //правило - плагины должны быть новыми инстансами классов(с ключевым словом new)
         new HTMLWebpackPlugin({
-            template: "./index.html"
+            template: "./index.html",
+            minify: {
+                collapseWhitespace: isProd
+            }
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new BundleAnalyzerPlugin()
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {}
+                    },
+                    "css-loader"
+                ]
             },
             {
                 test: /\.mp3$/,
